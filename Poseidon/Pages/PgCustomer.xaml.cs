86156432badbee759c9d05377ed2712752d48cf1
Salman Khan <mr.salman.rao@gmail.com>
2017-04-25 21:22:58 +0500

    Added Customer and Supplier

--- Poseidon/Pages/PgCustomer.xaml.cs
+++ Poseidon/Pages/PgCustomer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PoseidonShared.Model;
+﻿using Poseidon.Windows;
+using PoseidonShared.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,27 +15,234 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Poseidon
+namespace Poseidon.Pages
 {
     /// <summary>
     /// Interaction logic for PgCustomer.xaml
     /// </summary>
     public partial class PgCustomer : Page
     {
+        PoseidonDBEntities db = new PoseidonDBEntities();
+        Customer objCustomer = new Customer();
         public PgCustomer()
         {
             InitializeComponent();
+            txtcustomerName.Focus();
+            RefreshDataGrid();
         }
 
-        private void btnEditCustomer_Click(object sender, RoutedEventArgs e)
+        private void dgbtnEdit_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                objCustomer = new Customer();
+                this.clearfields();
+                objCustomer = (Customer)dgCustomer.SelectedItem;
+
+                if (objCustomer == null || objCustomer.Id < 1)
+                    return;
+
+                txtcustomerName.Text = objCustomer.CustomerName;
+                txtCustomerAge.Text = objCustomer.Age.ToString();
+                txtCustomerAddress.Text = objCustomer.Address;
+                txtCustomerCell.Text = objCustomer.Cell;
+                txtCustomerEmail.Text = objCustomer.Email;
+                txtCustomerGender.Text = objCustomer.Sex;
+                txtCustomerPhone.Text = objCustomer.Phone;
+                txtOrganization.Text = objCustomer.OrganizationName;
+                dpRegistrationDate.Text = objCustomer.RegDate.ToString();
+                txtCategory.Text = objCustomer.Category;
+                txtBimaNo.Text = objCustomer.BimaNo;
+                txtCredit.Text = objCustomer.Credit.ToString();
+                txtDebit.Text = objCustomer.Debit.ToString();
+
+
+                chkDOU.IsChecked = objCustomer.Dou;
+                chkMedicalStore.IsChecked = objCustomer.MedicalStore;
+                btnEdit.Visibility = Visibility.Visible;
+                EditLabel.Content = "Update in Progress,\n Click to cancel.";
+                btnAdd.IsEnabled = false;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        private void dgbtnDelete_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Customer row = (Customer)dgCustomer.SelectedItem;
+
+                if (row == null || row.Id < 1)
+                    return;
+                if (MessageBox.Show(String.Format("Delete '{0}' ?", row.CustomerName), "Confirm delete", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.No)
+                    return;
+                db.Customers.Remove(row);
+                db.SaveChanges();
+                this.RefreshDataGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
+
+        private void clearfields()
+        {
+            txtcustomerName.Text = String.Empty;
+            txtCustomerAge.Text = String.Empty;
+            txtCustomerGender.Text = String.Empty;
+            txtCustomerAddress.Text = String.Empty;
+            txtCustomerPhone.Text = String.Empty;
+            txtCustomerCell.Text = String.Empty;
+            txtCustomerEmail.Text = String.Empty;
+            txtOrganization.Text = String.Empty;
+            chkMedicalStore.IsChecked = false;
+            chkDOU.IsChecked = false;
+            txtBimaNo.Text = String.Empty;
+            txtCategory.Text = String.Empty;
+            dpRegistrationDate.Text = String.Empty;
+            txtCredit.Text = String.Empty;
+            txtDebit.Text = String.Empty;
+        }
+
+        private void RefreshDataGrid()
+        {
+            try
+            {
+                List<Customer> lstCstomer = db.Customers.ToList();
+                dgCustomer.ItemsSource = lstCstomer;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtcustomerName.Text == String.Empty)
+                {
+                    MessageBox.Show("Please enter customer name first.");
+                    return;
+                }
+                objCustomer = new Customer();
+                objCustomer.CustomerName = txtcustomerName.Text;
+                objCustomer.Age =Convert.ToInt32( txtCustomerAge.Text);
+                objCustomer.Sex = txtCustomerGender.Text;
+       
+                objCustomer.Address = txtCustomerAddress.Text;  //datatype = byte[]
+                objCustomer.Phone = txtCustomerPhone.Text;
+                objCustomer.Cell = txtCustomerCell.Text;
+                objCustomer.Email = txtCustomerEmail.Text;
+                objCustomer.OrganizationName = txtOrganization.Text;
+                objCustomer.MedicalStore = Convert.ToBoolean(chkMedicalStore.IsChecked);   //datatype== bool
+                objCustomer.Dou = Convert.ToBoolean(chkDOU.IsChecked);  // datatype bool
+                objCustomer.BimaNo = txtBimaNo.Text;
+
+                if (txtCredit.Text == String.Empty)
+                {
+                    txtCredit.Text = Convert.ToString(0);
+                }
+                if (txtDebit.Text == String.Empty)
+                {
+                    txtDebit.Text = Convert.ToString(0);
+                }
+                objCustomer.Credit = Convert.ToDecimal(txtCredit.Text);
+                objCustomer.Debit = Convert.ToDecimal(txtDebit.Text);
+
+                objCustomer.Category = txtCategory.Text;
+                // String date = Convert.ToDateTime(txtRegDate.Text).ToString("dd-MMM-yyyy");
+                String date = Convert.ToDateTime(dpRegistrationDate.Text).ToString("dd-MMM-yyyy");
+                objCustomer.RegDate = Convert.ToDateTime(date);
+                db.Customers.Add(objCustomer);
+                db.SaveChanges();
+                RefreshDataGrid();
+                clearfields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var id = objCustomer.Id;
+
+                var result = db.Customers.SingleOrDefault(b => b.Id == id);
+                result.CustomerName = txtcustomerName.Text;
+                result.Age = Convert.ToInt32(txtCustomerAge.Text);
+                result.Address = txtCustomerAddress.Text;
+                result.BimaNo = txtBimaNo.Text;
+                result.Category = txtCategory.Text;
+                result.Cell = txtCustomerCell.Text;
+                result.Sex = txtCustomerGender.Text;
+                result.Email = txtCustomerEmail.Text;
+                result.MedicalStore = Convert.ToBoolean(chkMedicalStore.IsChecked);   //datatype== bool
+                result.Dou = Convert.ToBoolean(chkDOU.IsChecked);
+                result.Phone = txtCustomerPhone.Text;
+                result.Category = txtCategory.Text;
+                String date = Convert.ToDateTime(dpRegistrationDate.Text).ToString("dd-MMM-yyyy");
+                result.RegDate = Convert.ToDateTime(date);
+                if (txtCredit.Text == String.Empty)
+                {
+                    txtCredit.Text = Convert.ToString(0);
+                }
+                if (txtDebit.Text == String.Empty)
+                {
+                    txtDebit.Text = Convert.ToString(0);
+                }
+                result.Credit = Convert.ToDecimal(txtCredit.Text);
+                result.Debit = Convert.ToDecimal(txtDebit.Text);
+
+                db.SaveChanges();
+                RefreshDataGrid();
+                clearfields();
+                objCustomer = null;
+                btnAdd.IsEnabled = true;
+                btnEdit.Visibility = Visibility.Hidden;
+                EditLabel.Content = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void EditLabel_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                objCustomer = null;
+                btnAdd.IsEnabled = true;
+                btnEdit.Visibility = Visibility.Hidden;
+                EditLabel.Content = "";
+                this.clearfields();
+            }
+            catch(Exception ex )
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
     }
 }
 
--- Poseidon/Pages/PgSupplier.xaml.cs
+++ Poseidon/Pages/PgSupplier.xaml.cs
@@ -1,4 +1,6 @@
-﻿using PoseidonShared.Model;
+﻿using Poseidon.Windows;
+using PoseidonShared.Model;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,27 +16,247 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Poseidon
+namespace Poseidon.Pages
 {
     /// <summary>
     /// Interaction logic for PgSupplier.xaml
     /// </summary>
     public partial class PgSupplier : Page
     {
+        PoseidonDBEntities db = new PoseidonDBEntities();
+        Supplier objSupplier = new Supplier();
         public PgSupplier()
         {
             InitializeComponent();
+            RefreshDataGrid();
+            txtSupplierName.Focus();
         }
 
+        //grid edit supplier 
         private void btnEditSupplier_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                objSupplier = null;
+                this.clearFields();
+                objSupplier = (Supplier)dgSupplier.SelectedItem;
+                txtSupplierName.Text = objSupplier.SupplierName;
+                txtPhoneNo.Text = objSupplier.Phone;
+                txtAddress.Text = objSupplier.Address;
+                txtCountry.Text = objSupplier.Country;
+                txtEmail.Text = objSupplier.Email;
+                btnAdd.IsEnabled = false;
+                btnEdit.Visibility = Visibility.Visible;
+                EditLabel.Content = "Update in Progress,\n Click to cancel.";
+                btnDone.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
+        }
+
+        private void clearFields()
+        {
+            try
+            {
+                txtSupplierName.Text = String.Empty;
+                txtPhoneNo.Text = String.Empty;
+                txtAddress.Text = String.Empty;
+                txtCountry.Text = String.Empty;
+                txtEmail.Text = String.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Supplier row = (Supplier)dgSupplier.SelectedItem;
+
+                if (row == null || row.Id < 1)
+                    return;
+                if (MessageBox.Show(String.Format("Delete '{0}' ?", row.SupplierName), "Confirm delete", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.No)
+                    return;
+                db.Suppliers.Remove(row);
+                db.SaveChanges();
+                this.RefreshDataGrid();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void RefreshDataGrid()
+        {
+            try
+            {
+                List<Supplier> lstSupplier = db.Suppliers.ToList();
+                dgSupplier.ItemsSource = lstSupplier;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAddSupplier_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtSupplierName.Text == "")
+                {
+                    MessageBox.Show("Please enter supplier name first.");
+                    return;
+                }
+                var supplier = new Supplier();
+                supplier.SupplierName = txtSupplierName.Text;
+                supplier.Phone = txtPhoneNo.Text;
+                supplier.Address = txtAddress.Text;
+                supplier.Country = txtCountry.Text;
+                supplier.Email = txtEmail.Text;
+                db.Suppliers.Add(supplier);
+                db.SaveChanges();
+                this.RefreshDataGrid();
+                clearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
+       // public virtual bool Update(T entity)
+        //{
+            //Decimal id = ((dynamic)entity).Id;
+            //var dbObject = ObjectSet.Where("it.Id=" + id).FirstOrDefault();
+            //var entry = Db.Entry(dbObject);
+            //entry.CurrentValues.SetValues(entity);
+            //entry.State = EntityState.Modified;
+            //Db.SaveChanges();
+            //return true;
+        //}
+        private void dgSupplier_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void EditLabel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                objSupplier = null;
+                btnAdd.IsEnabled = true;
+                btnEdit.Visibility = Visibility.Hidden;
+                EditLabel.Content = "";
+                this.clearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var id = objSupplier.Id;
+                var result = db.Suppliers.SingleOrDefault(b => b.Id == id);
+                result.SupplierName = txtSupplierName.Text;
+                result.Phone = txtPhoneNo.Text;
+                result.Address = txtAddress.Text;
+                result.Country = txtCountry.Text;
+                result.Email = txtEmail.Text;
+                db.SaveChanges();
+                RefreshDataGrid();
+                clearFields();
+                objSupplier = null;
+                btnAdd.IsEnabled = true;
+                btnEdit.Visibility = Visibility.Hidden;
+                EditLabel.Content = "";
+
+                //this.save()
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void dgSupplier_RowEditEnding_1(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            try
+            {
+                /// Handle Updates or Creates
+                if (e.EditAction == DataGridEditAction.Commit)
+                {
+                    // Get the Customer changes from the DataGrid
+                    Supplier supplier = e.Row.DataContext as Supplier;
+
+
+                    //if (String.IsNullOrEmpty(customerGroup.CustomerGroupName))
+                    //{
+                    //    MessageBox.Show("Must Provide a Customer Group Name", "Customer Group Name", MessageBoxButton.OK, MessageBoxImage.Information);
+                    //    this.BindData();
+                    //    return;
+                    //}
+
+
+
+                    if ((MessageBox.Show("Are you sure you want to add/modify '" + supplier.SupplierName + "' ?", "Confirm add or modify", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes) != MessageBoxResult.Yes))
+                    {
+                        e.Cancel = true;
+                        //this.RefreshDataGrid();
+                        return;
+                    }
+                    //if (supplier.Id < 1)
+                    //{
+                    //    var Repo = new RepositorySupplier().Add(supplier);
+                    //    if (Repo)
+                    //    {
+                    //        MessageBox.Show("Added successfully");
+                    //    }
+
+                    //    //ParaUtils.ShowStatusBarMsg(this, "Added CustomerGroup '" + customerGroup.CustomerGroupName + "'");
+                    //    //this.ShowCustomerGroupDialog((from c in BobShared.BobSharedUtils.GetDBDataContext().CustomerGroups where c.CustomerGroupId == customerGroup.CustomerGroupId select c).SingleOrDefault());
+                    //}
+                    //else
+                    //{
+                    //    var Repo = new RepositorySupplier().Update(supplier);
+                    //    if (Repo)
+                    //    {
+                    //        MessageBox.Show("Updated successfully");
+                    //    }
+                    //    //ParaUtils.ShowStatusBarMsg(this, "Updated Customer Group '" + customerGroup.CustomerGroupName + "'");
+                    //}
+                    // this.BindData();
+                }
+            }
+            catch (Exception ex)
+            {
+                //  BobSharedUtils.HandleException(ex);
+                MessageBox.Show(ex.Message);
+                e.Cancel = true;
+            }
+        }
+
+
+
     }
 }
 
